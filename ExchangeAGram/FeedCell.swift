8f84f849ba9a366d740c9ead860cd11bf6f0ae43
Jim Veneskey <github@strifecrafter.com>
2015-06-11 13:01:26 -0400

    Customizing the prototype cell

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by jim Veneskey on 6/11/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
